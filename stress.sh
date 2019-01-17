83522d5896c4ae1c5c3b93a6074d8ab118e963fa
Samvel Abrahamyan <samvel1024@gmail.com>
2019-01-17 22:59:41 +0100

    Time the test

--- stress.sh
+++ stress.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
+ctr=0
 
 while [[ true ]]; do
+    ctr=$((ctr + 1))
+
     players=$(shuf -i 2-$1 -n 1)
     rooms=$(shuf -i 1-$2 -n 1)
     count=$(shuf -i 1-$3 -n 1)
+    echo "************************ TEST $ctr ***********************"
     echo "Testing with $players players $rooms rooms $count requests"
     ./test.sh "$players $rooms $count"
 done;
